1eeba2051cfbf4c8c7e08c87b5e396dc8de865ea
Steven Herrera <stev1998@hotmail.com>
2020-08-20 14:32:31 -0500

    Creacion de Api

--- controllers/bicicleta.js
+++ controllers/bicicleta.js
@@ -1,5 +1,4 @@
 var Bicicleta = require('../models/bicicleta');
-const { findById } = require('../models/bicicleta');
 
 exports.bicicleta_list = function(req, res){
     res.render('bicicletas/index', {bicis: Bicicleta.allBicis});
--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -4,8 +4,13 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([-2.2287768,-79.9354746]).addTo(map);
-L.marker([-2.227201,-79.9394636]).addTo(map);
-L.marker([-2.222468,-79.936486]).addTo(map);
-
-
+$.ajax({
+    dataType: "json",
+    url:"api/bicicletas",
+    success: function(result){
+        console.log(result);
+        result.bicicletas.forEach(function(bici){
+            L.marker(bici.ubicacion, {title: bici.id}).addTo(map);
+        });
+    }
+})
